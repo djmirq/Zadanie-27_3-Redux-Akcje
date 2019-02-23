7d0ac31acee3a5094a4a765fd686e942781ce0bf
djmirq <36812301+djmirq@users.noreply.github.com>
2019-02-23 17:42:32 +0100

    first commit

---
+++ action.js
@@ -0,0 +1,48 @@
+import uuid from uuid;
+
+ 
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+ 
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text: text        
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id        
+    }
+}
+
+function thumbUpComment(id, handup) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        handup: handup++
+    }
+}
+
+function thumbDownComment(id, handdown) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        handdown: handdown--
+    }
+}
